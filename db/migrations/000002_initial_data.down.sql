74b8917035ee46d38855e1a9d63067cb5f3e2d0c
take73 <shun.takenami@petokoto.com>
2024-12-10 09:51:18 +0900

    add initial data

---
+++ db/migrations/000002_initial_data.down.sql
@@ -0,0 +1,11 @@
+-- 外部キー制約を無効化
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- テーブルデータを削除 (すべての行を削除し、AUTO_INCREMENT をリセット)
+TRUNCATE TABLE client_bank_account;
+TRUNCATE TABLE client;
+TRUNCATE TABLE user;
+TRUNCATE TABLE organization;
+
+-- 外部キー制約を有効化
+SET FOREIGN_KEY_CHECKS = 1;
